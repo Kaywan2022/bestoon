d74d707b3e8adbc3cce7a6fac6d079552f0f5968
Kaywan2022 <100763235+Kaywan2022@users.noreply.github.com>
2022-12-29 13:51:43 +0330

    merge clients scripts

--- bestoon/clients/commandline/bestoonexpense.sh
+++ bestoon/clients/commandline/bestoonexpense.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# source "$(dirname $0)"/bestoonconfig.sh
+source "$(dirname $0)"/config.sh
 
-# print_usage()
-# {
-#     echo "Use this script to submit expense reports to ${BASE_URL}"
-#     echo "Usage: ${0} <Amount> <Description>. Eg:"
-#     echo "Usage: ${0} 1000 Donation"
-# }
+print_usage()
+{
+    echo "Use this script to submit expense reports to ${BASE_URL}"
+    echo "Usage: ${0} <Amount> <Description>. Eg:"
+    echo "Usage: ${0} 1000 Donation"
+}
 
-# AMOUNT=$1
-# shift
-# TEXT=$*
-# if [ -z "$TEXT" ]; then
-#     print_usage
-#     exit 1
-# fi
-
-#please set this
-source config.sh
+AMOUNT=$1
+shift
+TEXT=$*
+if [ -z "$TEXT" ]; then
+    print_usage
+    exit 1
+fi
 
 curl --data "token=$TOKEN&amount=$1&text=$2" $BASE_URL/submit/expense/
+
+#please set this
+# source config.sh
+# curl --data "token=$TOKEN&amount=$1&text=$2" $BASE_URL/submit/expense/
--- bestoon/clients/commandline/bestoonincome.sh
+++ bestoon/clients/commandline/bestoonincome.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# source "$(dirname $0)"/bestoonconfig.sh
+source "$(dirname $0)"/bestoonconfig.sh
 
-# print_usage()
-# {
-#     echo "Use this script to submit income reports to ${BASE_URL}"
-#     echo "Usage: ${0} <Amount> <Description>. Eg:"
-#     echo "Usage: ${0} 1000 Donation"
-# }
+print_usage()
+{
+    echo "Use this script to submit income reports to ${BASE_URL}"
+    echo "Usage: ${0} <Amount> <Description>. Eg:"
+    echo "Usage: ${0} 1000 Donation"
+}
 
-# AMOUNT=$1
-# shift
-# TEXT=$*
-# if [ -z "$TEXT" ]; then
-#     print_usage
-#     exit 1
-# fi
+AMOUNT=$1
+shift
+TEXT=$*
+if [ -z "$TEXT" ]; then
+    print_usage
+    exit 1
+fi
 
-# curl --data "token=$TOKEN&amount=$AMOUNT&text=$TEXT" $BASE_URL/submit/income/
+curl --data "token=$TOKEN&amount=$AMOUNT&text=$TEXT" $BASE_URL/submit/income/
 
 #please set this
-source config.sh
+# source config.sh
 
-curl --data "token=$TOKEN&amount=$1&text=$2" $BASE_URL/submit/income/
+# curl --data "token=$TOKEN&amount=$1&text=$2" $BASE_URL/submit/income/
